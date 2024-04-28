b0e5e908f7206a6799faa74b3229b0788208f114
EzzineMehdi <165754720+EzzineMehdi@users.noreply.github.com>
2024-04-28 02:34:05 +0100

    raycasting.c

--- raycasting.c
+++ raycasting.c
@@ -5,10 +5,10 @@ void    create_square(t_img_data *img, size_t ray_l, size_t ray_c, size_t n)
     size_t y = (WIDTH / 2) - (ray_l / 2);
     size_t x;
 
-    while (y < (WIDTH / 2) + (ray_l / 2))
+    while (y <= (WIDTH / 2) + (ray_l / 2))
     {
         x = HEIGHT / ray_c * n;
-        while (x < (HEIGHT / ray_c) * (n + 1))
+        while (x <= (HEIGHT / ray_c) * (n + 1))
         {
             my_mlx_pixel_put(img, x, y, 0xFFFFFF);
             x ++;

a6dd6e05f4b11f4c1985cc934f32e201ee9448db
7h3Y055 <youssefbouchmama18@outlook.com>
2024-05-15 12:28:59 +0100

    norm ft_parse.c

--- ft_parse.c
+++ ft_parse.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybouchma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 12:19:44 by ybouchma          #+#    #+#             */
+/*   Updated: 2024/05/15 12:28:23 by ybouchma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 int	check_valid_map(t_ptr *ptr, int y, int x)
@@ -6,12 +18,12 @@ int	check_valid_map(t_ptr *ptr, int y, int x)
 			+ 1][x] == ' '))
 		return (1);
 	else if (y > 0 && (!ptr->map2d[y - 1] || (ptr->map2d[y - 1][x] == '\0'
-				|| ptr->map2d[y - 1][x] == ' ')))
+			|| ptr->map2d[y - 1][x] == ' ')))
 		return (2);
 	else if (ptr->map2d[y][x + 1] == '\0' || ptr->map2d[y][x + 1] == ' ')
 		return (3);
-	else if (x > 0 && (ptr->map2d[y][x - 1] == '\0' || ptr->map2d[y][x
-			- 1] == ' '))
+	else if (x > 0 && (ptr->map2d[y][x - 1] == '\0' || ptr->map2d[y][
+		x - 1] == ' '))
 		return (4);
 	return (0);
 }
@@ -20,6 +32,34 @@ void	ft_init_obunga_position(t_ptr *ptr, int y, int x)
 {
 	ptr->obunga.y = y * SCALE + SCALE / 2;
 	ptr->obunga.x = x * SCALE + SCALE / 2;
+}
+
+void	check_valide_map_helper(t_ptr *ptr, int y, int x, int *n)
+{
+	if (ptr->map2d[y][x] == '0' && check_valid_map(ptr, y, x))
+		exit(ft_error(ptr,
+				"Invalid map: map is not closed by wall (1)\n",
+				check_valid_map(ptr, y, x)));
+	if (ptr->map2d[y][x] == 'X')
+	{
+		ft_init_obunga_position(ptr, y, x);
+		ptr->map2d[y][x] = '0';
+		(*n)++;
+	}
+	else if (ptr->map2d[y][x] == 'D')
+	{
+		if (!((ptr->map2d[y][x - 1] == '1' && ptr->map2d[y][x
+				+ 1] == '1') || (ptr->map2d[y - 1][x] == '1'
+					&& ptr->map2d[y + 1][x] == '1')))
+			exit(ft_error(ptr, "door error\n", 1));
+	}
+	else if (ptr->map2d[y][x] && ptr->map2d[y][x] != '0'
+		&& ptr->map2d[y][x] != '1' && ptr->map2d[y][x] != ' ')
+	{
+		printf("%c\n", ptr->map2d[y][x]);
+		exit(ft_error(ptr, "Invalid map: contain invalid character!\n",
+				check_valid_map(ptr, y, x)));
+	}
 }
 
 void	check_valide_map(t_ptr *ptr)
@@ -35,30 +75,7 @@ void	check_valide_map(t_ptr *ptr)
 		x = 0;
 		while (x < ptr->parse.x)
 		{
-			if (ptr->map2d[y][x] == '0' && check_valid_map(ptr, y, x))
-				exit(ft_error(ptr,
-						"Invalid map: map is not closed by wall (1)\n",
-						check_valid_map(ptr, y, x)));
-			if (ptr->map2d[y][x] == 'X')
-			{
-				ft_init_obunga_position(ptr, y, x);
-				ptr->map2d[y][x] = '0';
-				n++;
-			}
-			else if (ptr->map2d[y][x] == 'D')
-			{
-				if (!((ptr->map2d[y][x - 1] == '1' && ptr->map2d[y][x
-							+ 1] == '1') || (ptr->map2d[y - 1][x] == '1'
-							&& ptr->map2d[y + 1][x] == '1')))
-					exit(ft_error(ptr, "door error\n", 1));
-			}
-			else if (ptr->map2d[y][x] && ptr->map2d[y][x] != '0'
-				&& ptr->map2d[y][x] != '1' && ptr->map2d[y][x] != ' ')
-			{
-				printf("%c\n", ptr->map2d[y][x]);
-				exit(ft_error(ptr, "Invalid map: contain invalid character!\n",
-						check_valid_map(ptr, y, x)));
-			}
+			check_valide_map_helper(ptr, y, x, &n);
 			x++;
 		}
 		y++;
@@ -81,5 +98,4 @@ void	ft_parse(t_ptr *ptr, int argc, char const **argv)
 	fd = ft_open(ptr, argv[1]);
 	ft_init(ptr, fd);
 	check_valide_map(ptr);
-	// create_scaled_map(ptr);
 }
